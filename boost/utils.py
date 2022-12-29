0bf61b06612753f9b9654f3aaafdf1d095e69558
Jeton Kukalaj <jeton.kukalaj@gmail.com>
2022-12-30 00:27:56 +0100

    add refactoring

--- boost/utils.py
+++ boost/utils.py
@@ -1,11 +1,14 @@
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Optional
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import KFold, StratifiedKFold
 from boost.problem import ProblemType
 
 
-def inject_idx(df: pd.DataFrame, idx: int) -> pd.DataFrame:
+def inject_idx(df: pd.DataFrame, idx: Optional[int]) -> pd.DataFrame:
+    if idx is None:
+        return df
+
     if idx not in df.columns:
         df[idx] = np.arange(len(df))
 
@@ -13,45 +16,42 @@ def inject_idx(df: pd.DataFrame, idx: int) -> pd.DataFrame:
 
 
 def create_folds(
-        train_df: pd.DataFrame,
+        df: pd.DataFrame,
         targets: List[str],
         num_folds: int,
         problem: ProblemType,
         seed: int,
 ) -> pd.DataFrame:
-    # TODO: What about this?
-    # https://github.com/abhishekkrthakur/autoxgb/blob/334a0410466fbbd68d0f7c67acc2cb949b3d6fdc/src/autoxgb/autoxgb.py#L53
-
-    train_df["kfold"] = -1
+    df["kfold"] = -1
     if problem in (ProblemType.binary_classification, ProblemType.multi_class_classification):
-        y = train_df[targets].values
+        y = df[targets].values
         kf = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=seed)
-        for fold, (_, valid_indices) in enumerate(kf.split(X=train_df, y=y)):
-            train_df.loc[valid_indices, "kfold"] = fold
+        for fold, (_, valid_indices) in enumerate(kf.split(X=df, y=y)):
+            df.loc[valid_indices, "kfold"] = fold
     elif problem == ProblemType.single_column_regression:
-        y = train_df[targets].values
-        num_bins = int(np.floor(1 + np.log2(len(train_df))))
+        y = df[targets].values
+        num_bins = int(np.floor(1 + np.log2(len(df))))
         if num_bins > 10:
             num_bins = 10
         kf = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=seed)
-        train_df["bins"] = pd.cut(train_df[targets].values.ravel(), bins=num_bins, labels=False)
-        for fold, (_, valid_indices) in enumerate(kf.split(X=train_df, y=y)):
-            train_df.loc[valid_indices, "kfold"] = fold
-        train_df = train_df.drop("bins", axis=1)
+        df["bins"] = pd.cut(df[targets].values.ravel(), bins=num_bins, labels=False)
+        for fold, (_, valid_indices) in enumerate(kf.split(X=df, y=y)):
+            df.loc[valid_indices, "kfold"] = fold
+        df = df.drop("bins", axis=1)
     elif problem == ProblemType.multi_column_regression:
-        y = train_df[targets].values
+        y = df[targets].values
         kf = KFold(n_splits=num_folds, shuffle=True, random_state=seed)
-        for fold, (_, valid_indices) in enumerate(kf.split(X=train_df, y=y)):
-            train_df.loc[valid_indices, "kfold"] = fold
+        for fold, (_, valid_indices) in enumerate(kf.split(X=df, y=y)):
+            df.loc[valid_indices, "kfold"] = fold
     elif problem == ProblemType.multi_label_classification:  # TODO: use `iterstrat`
-        y = train_df[targets].values
+        y = df[targets].values
         kf = KFold(n_splits=num_folds, shuffle=True, random_state=seed)
-        for fold, (_, valid_indices) in enumerate(kf.split(X=train_df, y=y)):
-            train_df.loc[valid_indices, "kfold"] = fold
+        for fold, (_, valid_indices) in enumerate(kf.split(X=df, y=y)):
+            df.loc[valid_indices, "kfold"] = fold
     else:
         raise Exception("Problem type not supported")
 
-    return train_df
+    return df
 
 
 def dict_mean(dict_list) -> Dict[str, Any]:
